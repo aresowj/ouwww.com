217d37ecc3735c7d22d89f94e9110732c7025e34
Weijie Ou <ares.ou@outlook.com>
2026-09-12 00:07:46 -0700

    feat: use site repository for comments

--- src/lib/dates.js
+++ src/lib/dates.js
@@ -1,3 +1,3 @@
-export function formatPostDate({ year, month, day }) {
-  return new Date(`${year}-${month}-${day}T12:00:00Z`).toLocaleDateString('en-US', { dateStyle: 'long', timeZone: 'UTC' });
+export function formatPostDate({ date, year, month, day }) {
+  return new Date(date ?? `${year}-${month}-${day}T12:00:00Z`).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', timeZone: 'UTC', timeZoneName: 'short' });
 }
